25391b29c3cb9161af604a77d2f7e431de68dc06
wei tian <ttyou23@126.com>
2019-05-20 17:14:53 +0800

    update

--- src/spider/thread_pool.go
+++ src/spider/thread_pool.go
@@ -2,9 +2,25 @@
 package spider
 
 import (
-	"fmt"
+	"sync"
 )
 
-func test() {
-	fmt.Println("Hello World!")
+var wg sync.WaitGroup
+
+// type ThreadManager struct {
+// 	var aw sync.WaitGroup
+// }
+
+func Start_work(f func(map[string]string), param map[string]string, cout int) {
+
+	wg.Add(cout)
+	for i := 0; i < cout; i++ {
+		go working(f, param)
+	}
+	wg.Wait()
+}
+
+func working(f func(map[string]string), param map[string]string) {
+	f(param)
+	wg.Done()
 }
